67ab93258bbdd903737dff50f2fc07696c694380
Danieleriksson23 <59643211+Danieleriksson23@users.noreply.github.com>
2021-01-06 11:33:01 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace webcamproj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(424, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(510, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 360);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(646, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Media;
+using System.Threading;
+using System.IO;
+
+
+
+namespace webcamproj
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            var fileSystemWatcher = new FileSystemWatcher();
+
+            fileSystemWatcher.Changed += FileSystemWatcher_changed;
+
+            fileSystemWatcher.Path = @"C:\Users\daniel.eriksson23\Google Drive";
+
+            fileSystemWatcher.EnableRaisingEvents = true;
+            
+
+
+        }
+        private void FileSystemWatcher_changed(object sender, FileSystemEventArgs e)
+        {
+            label1.Invoke(new Action(() => label1.Text = "random123"));
+            this.Invoke((MethodInvoker)delegate ()
+            {
+
+                CaptureDevice = new VideoCaptureDevice(InfoCollection[comboBox1.SelectedIndex].MonikerString);
+            });
+            // skapar en funktion för att kunna starta kameran och sedan visa vad kameran ser
+            
+            CaptureDevice.NewFrame += CaptureDevice_newframe;
+
+            CaptureDevice.Start();;
+
+        }
+
+
+        FilterInfoCollection InfoCollection;
+        VideoCaptureDevice CaptureDevice;
+        CameraControlProperty cameracontrol;
+      
+        
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CaptureDevice = new VideoCaptureDevice(InfoCollection[comboBox1.SelectedIndex].MonikerString);
+            // skapar en funktion för att kunna starta kameran och sedan visa vad kameran ser
+            CaptureDevice.NewFrame += CaptureDevice_newframe;
+            CaptureDevice.Start();
+            
+        }
+        private void CaptureDevice_newframe(object sender, NewFrameEventArgs eventArgs)
+        {
+            // visar vad kameran ser i pictureboxen
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //när applikationen stängs så avaktiveras capturedevice(kameran) 
+            if (CaptureDevice.IsRunning == true)
+                CaptureDevice.Stop();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+          
+
+            InfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            //skriv alla tillgängliga kameror i en combobox
+            foreach (FilterInfo Device in InfoCollection)
+                comboBox1.Items.Add(Device.Name);
+            comboBox1.SelectedIndex = 0;
+            CaptureDevice = new VideoCaptureDevice();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
